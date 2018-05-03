303241319b5d2cc7aa579e9e439171ef949b6abe
Usuario <luisfgaravil@gmail.com>
2018-05-03 13:51:21 -0500

    Imagencita agregadita

--- app/src/main/java/com/example/usuario/ejemplo_git/MainActivity.java
+++ app/src/main/java/com/example/usuario/ejemplo_git/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.usuario.ejemplo_git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public ImageView imagencita;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //comentario
+
     }
 }
